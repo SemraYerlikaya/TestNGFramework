413bcab92ffe624bb1e167fa54b16f4d2308b539
SemraYerlikaya <70419562+SemraYerlikaya@users.noreply.github.com>
2020-11-25 22:39:57 -0800

    Update AmazonAramaTest.java

--- src/test/java/com/techproed/tests/AmazonAramaTest.java
+++ src/test/java/com/techproed/tests/AmazonAramaTest.java
@@ -26,6 +26,7 @@ public class AmazonAramaTest extends TestBaseFinal{
         extentTest.info("Assert islemi yapiyoruz");
 
         Assert.assertTrue(yazilar.contains(ConfigurationReader.getProperty("aranilacak_kelime")));
+        System.out.print;n("Denene");
         extentTest.pass("Testimiz PASSED !");
     }
 
